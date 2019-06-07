9b389618e80d46c47874e76802df043fd479c3f5
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-06-07 08:53:25 +0100

    Clean up plan

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,7 +96,7 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Initial plan:
+        # Plan:
 
         # Use the light to determine when the sorting has completed.
         # The steps would be to turn on the light, pick up an item and start moving right.
@@ -104,13 +104,13 @@ class SortingRobot:
         #   - Turn on the robot's light.
         # While the robot can move right:
         #   - The first item will be None so swap the current item and move right.
-        #   - If the value of the held item is greater than the item in front of the robot, then swap the items, move left, and swap again.
+        #   - If the value of the held item is greater than the item in front of the robot, then swap the items,
+        #   move left, and swap again.
         #   - If there is nothing left to compare, then turn off the light.
         #   - Otherwise move left and swap the items to leave that item where it was an move right.
         # While the robot has reached the end of the list and can no longer move right and the light is turned off:
         #   - The robot should turn back to the starting point.
 
-        pass
 
 
 if __name__ == "__main__":
